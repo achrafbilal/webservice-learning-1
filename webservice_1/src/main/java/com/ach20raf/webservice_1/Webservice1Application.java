6ba61665daf60c4e9cbdc728c13b04f09ae8092b
achraf bilal <bilal_achraf@emsi-edu.ma>
2022-03-13 00:42:03 +0100

    Project created

---
+++ webservice_1/src/main/java/com/ach20raf/webservice_1/Webservice1Application.java
@@ -0,0 +1,13 @@
+package com.ach20raf.webservice_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice1Application.class, args);
+	}
+
+}
---
+++ webservice_1/src/test/java/com/ach20raf/webservice_1/Webservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ach20raf.webservice_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
